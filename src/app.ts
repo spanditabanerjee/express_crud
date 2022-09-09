62e8637f2e0441579a2dad600c475ecd3540caa4
spandita.banerjee <spanditabanerjee@gmail.com>
2022-09-09 15:49:39 +0530

    updated

--- src/app.ts
+++ src/app.ts
@@ -5,11 +5,8 @@ import data from './routes/data';
 import get from './routes/get';
 import insert from './routes/insert';
 import update from './routes/update';
-
-
 import "reflect-metadata";
 
-// import './queries';
 
 const app = express();
 app.use(express.json());
@@ -20,40 +17,9 @@ app.use('/', update);
 app.listen(2000);
 
 
-
-
-
-//    app.get('/',async function(req,res){
-
-// //    // delete a particular record
-// //     await userRepo.delete(2);
-// //     res.send("record deleted")
-
-// //     //update
-// //     await userRepo.update(2, {name: "Rajesh" , email: "rajesh@email.com"});
-// // //     res.json("record Updated");
-//  })
-
 export const AppDataSource = new DataSource(ormConfig);
 AppDataSource.initialize().then(() => {
     console.log("Database connected successfully");
     app.listen(3000, () => { console.log('Server Running'); })
 }).catch((err) => console.log("error while connecting database", err));
 
-
-// const db = require('./queries')
-// const port = 3000
-
-// app.get('/', (request, response) => {
-//   response.json({ info: 'Node.js, Express, and Postgres API' })
-// })
-
-
-// app.get('/users/:id', db.getUserById)
-// app.post('/users', db.createUser)
-// app.put('/users/:id', db.updateUser)
-// app.delete('/users/:id', db.deleteUser)
-
-// app.listen(port, () => {
-//   console.log(`App running on port ${port}.`)
-// })
--- src/routes/get.ts
+++ src/routes/get.ts
@@ -12,28 +12,3 @@ router.get('/:id', async function (req, res) {
 export default router;
 
 
-
-// import express, { response } from "express";
-
-// const router = express.Router();
-
-// // router.get('/',(req,res)=>{
-// // //     const allRecords = userRepo.find()
-
-
-// // // res.json(allRecords);
-
-// // });
-
-// const getUserById = (req: { params: { id: string; }; }, res: any) => {
-//     const id = parseInt(req.params.id)
-  
-//     pool.query('SELECT * FROM users WHERE id = $1', [id], (error: any, results: { rows: any; }) => {
-//       if (error) {
-//         throw error
-//       }
-//       response.status(200).json(results.rows)
-//     })
-//   }
-
-// export default router;
